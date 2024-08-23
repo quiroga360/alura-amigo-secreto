8c0cc8240e4fc3b3691be80aa2cc052eb8ead6da
Pablo Quiroga <pabloquiroga@outlook.com.br>
2024-08-23 08:00:45 -0300

    preciso descobrir uma solução para finalizar o sorteio sem que uma pessoa seja sorteada duas vezes

--- js/app.js
+++ js/app.js
@@ -1,29 +1,54 @@
-const listaDeAmigos = [];
 const listaDeAmigosIncluidos = document.querySelector('#lista-amigos');
 
 const adicionar = () => {
 
-    console.log(listaDeAmigosIncluidos);
-    
-    console.log(listaDeAmigos);
-    
     const amigoInput = document.querySelector('#nome-amigo').value;
-    console.log(amigoInput);
 
     if (amigoInput) {
 
-        listaDeAmigos.push(amigoInput);
+        if (listaDeAmigosIncluidos.innerHTML) {
+            // adicionar a aprtir do segundo nome na lista
+            listaDeAmigosIncluidos.innerHTML = listaDeAmigosIncluidos.innerHTML + ', ' + amigoInput;
 
-        console.log(listaDeAmigos);
-        console.log(listaDeAmigosIncluidos.innerHTML);
+        } else {
+            // adicionar primeiro nome na lista
+            listaDeAmigosIncluidos.innerHTML = listaDeAmigosIncluidos.innerHTML + amigoInput;
+
+        }
+
+    } else {
+        // input vazio
+        alert('Digite um nome.');
+
+    }
+
+};
+
+const sortear = () => {
+
+    if (listaDeAmigosIncluidos.innerHTML) {
+        // criar array com nomes para sortear
+        const nomesParaSortear = listaDeAmigosIncluidos.innerHTML.split(', ');
+
+        // o maior número sorteado deve ser igual ao tamanho do array (último índice)
+        const valorMaximo = nomesParaSortear.length - 1;
+
+        // o menor número sorteado deve ser igual a zero (primeiro índice do array)
+        const valorMinimo = 0;
 
         
 
+        const sorteio = Math.floor(Math.random() * (valorMaximo - valorMinimo + 1) + valorMinimo);
+        console.log(sorteio);
+        
+
+        //const limiteSorteio = nomesParaSortear.length;
+        alert(nomesParaSortear[sorteio])
+
     } else {
-        alert('Digite um nome.');
+        alert('Sem nomes para sortear');
     }
 
-    
-    
+
 
 }
